714bfb526adbdc7bc8822bb7c6d0600b6566958c
Thies Edeling <thies@te-con.nl>
2012-07-22 14:14:29 +0200

    Initial setup

--- app/controllers/dataimport/ImportController.scala
+++ app/controllers/dataimport/ImportController.scala
@@ -1,7 +1,9 @@
 package controllers.dataimport
 
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 
 object ImportController extends Controller {
-
+  def index = Action {
+    Ok(views.html.dataimport())
+  }
 }
