101fb09697c8aac627add7f5eccea2da8cd4c2c8
Thies Edeling <thies@te-con.nl>
2012-08-08 22:58:46 +0200

    beginning of json interface

--- app/controllers/dataimport/ImportController.scala
+++ app/controllers/dataimport/ImportController.scala
@@ -2,12 +2,19 @@ package controllers.dataimport
 
 import play.api.mvc.{Action, Controller}
 import dataimport.{DataImportStatus, DataImportManager}
+import play.api.libs.json.Json.toJson
 
 object ImportController extends Controller {
   def index = Action {
     val status: DataImportStatus = DataImportStatus() // DataImportManager.status()
 
-    Ok(views.html.status_page(if (status.finished) { "finished" } else if (status.started) { "started" } else { "idle"}))
+    Ok(views.html.status_page(if (status.finished) {
+      "finished"
+    } else if (status.started) {
+      "started"
+    } else {
+      "idle"
+    }))
   }
 
   def triggerImport = Action {
@@ -16,10 +23,20 @@ object ImportController extends Controller {
   }
 
   def status = Action {
-    println("fetching status")
     val status: DataImportStatus = DataImportManager.status()
-    println("fetched")
 
-    Ok(views.html.status_page(if (status.finished) { "finished" } else if (status.started) { "started" } else { "idle"}))
+    Ok(views.html.status_page(if (status.finished) {
+      "finished"
+    } else if (status.started) {
+      "started"
+    } else {
+      "idle"
+    }))
+  }
+
+  def fetchStatus() = Action { request =>
+   Ok(toJson(
+        Map("status" -> "OK", "message" -> ("Hello there"))
+      ))
   }
 }
